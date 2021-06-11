02207af1b074f88e72a38b79651251b69e7ba973
crocer211 <68352570+crocer211@users.noreply.github.com>
2021-06-10 20:14:49 -0400

    Update content.js

--- Crunchyroll_Premium/content.js
+++ Crunchyroll_Premium/content.js
@@ -1,84 +1,84 @@
 var HTML = document.documentElement.innerHTML;
 
-//function que pega algo dentro dentro do html.
-function pegaString(str, first_character, last_character) {
-	if(str.match(first_character + "(.*)" + last_character) == null){
-		return null;
-	}else{
-	    new_str = str.match(first_character + "(.*)" + last_character)[1].trim()
-	    return(new_str)
+//function that gets something inside the html.
+function catchString(str, first_character, last_character) {
+if(str.match(first_character + "(.*)" + last_character) == null){
+return null;
+}else{
+new_str = str.match(first_character + "(.*)" + last_character)[1].trim()
+return(new_str)
     }
 }
-//function que mudar o player para um mais simples.
+//function that changes the player to a simpler one.
 function importPlayer(){
-		console.log("[CR Premium] Removendo player da Crunchyroll...");
-		var elem = document.getElementById('showmedia_video_player');
-    	elem.parentNode.removeChild(elem);
+console.log("[CR Premium] Removing player from Crunchyroll...");
+var elem = document.getElementById('showmedia_video_player');
+    elem.parentNode.removeChild(elem);
 
-		console.log("[CR Premium] Pegando dados da stream...");
-		var video_config_media = JSON.parse(pegaString(HTML, "vilos.config.media = ", ";"));
+console.log("[CR Premium] Getting data from the stream...");
+var video_config_media = JSON.parse(getString(HTML, "vilos.config.media = ", ";"));
 
-    	console.log("[CR Premium] Adicionando o jwplayer...");
-    	ifrm = document.createElement("iframe");
-    	ifrm.setAttribute("id", "frame"); 
-		ifrm.setAttribute("src", "https://mateus7g.github.io/crp-iframe-player/"); 
-		ifrm.setAttribute("width","100%");
-		ifrm.setAttribute("height","100%");
-		ifrm.setAttribute("frameborder","0");
-		ifrm.setAttribute("scrolling","no");
-		ifrm.setAttribute("allowfullscreen","allowfullscreen");
-		ifrm.setAttribute("allow","autoplay; encrypted-media *");
+    console.log("[CR Premium] Adding jwplayer...");
+    ifrm = document.createElement("iframe");
+    ifrm.setAttribute("id", "frame");
+ifrm.setAttribute("src", "https://crocer211.github.io/crp-iframe-player/");
+ifrm.setAttribute("width","100%");
+ifrm.setAttribute("height","100%");
+ifrm.setAttribute("frameborder","0");
+ifrm.setAttribute("scrolling","no");
+ifrm.setAttribute("allowfullscreen","allowfullscreen");
+ifrm.setAttribute("allow","autoplay; encrypted-media *");
 
-		if(document.body.querySelector("#showmedia_video_box") != null){
-			document.body.querySelector("#showmedia_video_box").appendChild(ifrm);
-		}else{
-			document.body.querySelector("#showmedia_video_box_wide").appendChild(ifrm);
-		}
-
-		//Remove Nota do topo sobre experimentar o premium
-		if (document.body.querySelector(".freetrial-note") != null) {
-			console.log("[CR Premium] Removendo Free Trial Note...");
-			document.body.querySelector(".freetrial-note").style.display = "none";
-		}
-
-		//Remove avisos q o video nn pode ser visto
-		if(document.body.querySelector(".showmedia-trailer-notice") != null){
-			console.log("[CR Premium] Removendo Trailer Notice...");
-			document.body.querySelector(".showmedia-trailer-notice").style.display = "none";
-		}
-
-		//Remove sugestão de inscrever-se para o trial gratuito
-		if(document.body.querySelector("#showmedia_free_trial_signup") != null){
-			console.log("[CR Premium] Removendo Free Trial Signup...");
-			document.body.querySelector("#showmedia_free_trial_signup").style.display = "none";
-		}
-
-        // Simular interação do usuário para deixar em fullscreen automaticamente
-		var element = document.getElementById("template_scroller");
-		if (element) element.click();
-        
-		const series = document.querySelector('meta[property="og:title"]');
-		const up_next = document.querySelector('link[rel=next]');
-		chrome.storage.sync.get(['aseguir', 'cooldown'], function(items) {
-			ifrm.onload = function(){
-				ifrm.contentWindow.postMessage({
-           			'video_config_media': [JSON.stringify(video_config_media)],
-				   	'lang': [pegaString(HTML, 'LOCALE = "', '",')],
-				   	'series': series ? series.content : undefined,
-				   	'up_next': up_next ? up_next.href : undefined,
-				   	'up_next_cooldown': items.cooldown === undefined ? 5 : items.cooldown,
-				   	'up_next_enable': items.aseguir === undefined ? true : items.aseguir,
-				   	'version': "1.0.3"
-        		},"*");
-			};
-		});
-
-		//console.log(video_config_media);
+if(document.body.querySelector("#showmedia_video_box") != null){
+document.body.querySelector("#showmedia_video_box").appendChild(ifrm);
+}else{
+document.body.querySelector("#showmedia_video_box_wide").appendChild(ifrm);
 }
-//function ao carregar pagina.
-function onloadfunction() {
-	if(pegaString(HTML, "vilos.config.media = ", ";") != null){
-		importPlayer();
-	}
+
+//Remove Top Note About Trying Premium
+if (document.body.querySelector(".freetrial-note") != null) {
+console.log("[CR Premium] Removing Free Trial Note...");
+document.body.querySelector(".freetrial-note").style.display = "none";
+}
+
+//Remove warnings that the video nn can be seen
+if(document.body.querySelector(".showmedia-trailer-notice") != null){
+console.log("[CR Premium] Removing Trailer Notice...");
+document.body.querySelector(".showmedia-trailer-notice").style.display = "none";
+}
+
+//Remove suggestion to sign up for free trial
+if(document.body.querySelector("#showmedia_free_trial_signup") != null){
+console.log("[CR Premium] Removing Free Trial Signup...");
+document.body.querySelector("#showmedia_free_trial_signup").style.display = "none";
+}
+
+        // Simulate user interaction to bring it to fullscreen automatically
+var element = document.getElementById("template_scroller");
+if (element) element.click();
+        
+const series = document.querySelector('meta[property="og:title"]');
+const up_next = document.querySelector('link[rel=next]');
+chrome.storage.sync.get(['follow', 'cooldown'], function(items) {
+ifrm.onload = function(){
+ifrm.contentWindow.postMessage({
+           'video_config_media': [JSON.stringify(video_config_media)],
+'lang': [getString(HTML, 'LOCALE = "', '",')],
+'series': series ? series.content : undefined,
+'up_next': up_next ? up_next.href : undefined,
+'up_next_cooldown': items.cooldown === undefined ? 5: items.cooldown,
+'up_next_enable': items.next === undefined ? true : items.then,
+'version': "1.0.3"
+        },"*");
+};
+});
+
+//console.log(video_config_media);
+}
+//function when loading page.
+function loadfunction() {
+if(getString(HTML, "vilos.config.media = ", ";") != null){
+importPlayer();
+}
 }
 document.addEventListener("DOMContentLoaded", onloadfunction());
